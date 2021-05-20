074bc92d8ff17957e5266d79bf9782187ff82a74
Stuart Bowes <stuartbowes2000@gmail.com>
2021-05-20 11:28:55 +0100

    Add QuoteManager class

---
+++ GreatQuotes/Data/QuoteManager.cs
@@ -0,0 +1,29 @@
+﻿using GreatQuotes.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace GreatQuotes.Data
+{
+    public class QuoteManager
+    {
+        static readonly Lazy<QuoteManager> instance = new Lazy<QuoteManager>(() => new QuoteManager());
+
+        private IQuoteLoader loader;
+
+        public IList<GreatQuoteViewModel> Quotes { get; set; }
+
+        public static QuoteManager Instance { get => instance.Value; }
+
+        private QuoteManager()
+        {
+            loader = QuoteLoaderFactory.Create();
+            Quotes = new ObservableCollection<GreatQuoteViewModel>(loader.Load());
+        }
+
+        public void Save()
+        {
+            loader.Save(Quotes);
+        }
+    }
+}
